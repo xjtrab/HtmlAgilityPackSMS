3fbe1cd822a4bdf9d473e82ce2972645ca66e9db
Xiaojian.Tang <Xiaojian.Tang@partner.bmw.com>
2018-12-18 16:57:52 +0800

    Database scaffolding

---
+++ efContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace HtmlAgilityPackSMS
+{
+    public partial class efContext : DbContext
+    {
+        public efContext()
+        {
+        }
+
+        public efContext(DbContextOptions<efContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("Server=localhost;Port=3307;Database=ef;User=root;Password=wo113661;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
