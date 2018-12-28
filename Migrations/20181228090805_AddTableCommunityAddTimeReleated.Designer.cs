2d07fcb9b2d3d0da6d72e67405f34f98c870b00b
Xiaojian.Tang <Xiaojian.Tang@partner.bmw.com>
2018-12-28 17:09:17 +0800

    db change

--- Migrations/20181228090805_AddTableCommunityAddTimeReleated.Designer.cs
+++ Migrations/20181228090805_AddTableCommunityAddTimeReleated.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HtmlAgilityPackSMS;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HtmlAgilityPackSMS.Migrations
 {
     [DbContext(typeof(efContext))]
-    partial class efContextModelSnapshot : ModelSnapshot
+    [Migration("20181228090805_AddTableCommunityAddTimeReleated")]
+    partial class AddTableCommunityAddTimeReleated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
