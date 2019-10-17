532d708da261e253619d07c0c821470bf95d317a
Xiaojian.Tang <xiaojian.tang@partner.bmw.com>
2019-10-17 10:02:24 +0800

    CHANGE

--- Services/CommunityService.cs
+++ Services/CommunityService.cs
@@ -5,6 +5,7 @@ using HtmlAgilityPack;
 using HtmlAgilityPackSMS.Interfaces;
 using HtmlAgilityPackSMS.Services;
 using HtmlAgilityPack.CssSelectors.NetCore;
+using System.Collections.Generic;
 
 public class CommunityService : HostedService
 {
@@ -21,40 +22,46 @@ public class CommunityService : HostedService
         {
             try
             {
-                var html = @"https://wx.5i5j.com/xiaoqu/o3/";
 
-                HtmlWeb web = new HtmlWeb();
-                web.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0";
-                var htmlDoc = web.Load(html);
-                Community community =  new Community ();
-                // var nodehousetit = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[5]/div[1]/div[1]/div/span");
-                // var nodeprice = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[2]/div[1]/span");
-                // var nodeSellHouseCount = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[2]/div[2]/ul/li[5]/a");
-                // var nodeRentHouseCount = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[2]/div[2]/ul/li[6]/a");
-                // var nodeSeeCountRecentThirtyDays = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[2]/ul/li[1]/div[2]/p");
-                var houseNodes = htmlDoc.QuerySelectorAll("div.listCon");
-                foreach (var item in houseNodes)
+                List<string> list = new List<string>{
+                   @"https://wx.5i5j.com/xiaoqu/o3/",
+                @"https://wx.5i5j.com/xiaoqu/o3n2/"};
+                list.ForEach(item =>
                 {
-                    community = new Community();
-                    community.Name =  item.QuerySelector("h3 > a").InnerText;
-                    community.Price =  int.Parse(item.QuerySelector("div > div > p.redC > strong").InnerText);
-                    community.SellingCount =  int.Parse(item.QuerySelector("div > div > a > p.num > span").InnerText
-                        .Trim('\r').Trim('\n').Trim());
-                    int countIndex = item.QuerySelector("div.listCon > div > p.xqzs.clear > span").InnerText.IndexOf("\u5957");
-                    if(countIndex != -1)
-                    community.SelledOutLastMonth = int.Parse(item.QuerySelector("div.listCon > div > p.xqzs.clear > span").InnerText.Substring(10,countIndex - 10 - 6));    
-                    int rentCountIndex = item.QuerySelector("div > p.xqzs.clear > span:nth-child(4) > a").InnerText.IndexOf("\u5957");
-                    if(rentCountIndex !=-1)
-                    community.RentingCount = int.Parse(item.QuerySelector("div > p.xqzs.clear > span:nth-child(4) > a").InnerText.Substring(0,rentCountIndex - 0 - 6));    
+                    Process(item);
+                });
 
-                    dbStorage.SaveCommunity(community);
-                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
             await Task.Delay(1000 * 60 * 30);
+        }
+    }
+
+    private void Process(string html)
+    {
+        HtmlWeb web = new HtmlWeb();
+        web.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0";
+        var htmlDoc = web.Load(html);
+        Community community = new Community();
+        var houseNodes = htmlDoc.QuerySelectorAll("div.listCon");
+        foreach (var item in houseNodes)
+        {
+            community = new Community();
+            community.Name = item.QuerySelector("h3 > a").InnerText;
+            community.Price = int.Parse(item.QuerySelector("div > div > p.redC > strong").InnerText);
+            community.SellingCount = int.Parse(item.QuerySelector("div > div > a > p.num > span").InnerText
+                .Trim('\r').Trim('\n').Trim());
+            int countIndex = item.QuerySelector("div.listCon > div > p.xqzs.clear > span").InnerText.IndexOf("\u5957");
+            if (countIndex != -1)
+                community.SelledOutLastMonth = int.Parse(item.QuerySelector("div.listCon > div > p.xqzs.clear > span").InnerText.Substring(10, countIndex - 10 - 6));
+            int rentCountIndex = item.QuerySelector("div > p.xqzs.clear > span:nth-child(4) > a").InnerText.IndexOf("\u5957");
+            if (rentCountIndex != -1)
+                community.RentingCount = int.Parse(item.QuerySelector("div > p.xqzs.clear > span:nth-child(4) > a").InnerText.Substring(0, rentCountIndex - 0 - 6));
+
+            dbStorage.SaveCommunity(community);
         }
     }
 }
