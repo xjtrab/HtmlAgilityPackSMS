1f994d335ae792b3461c395c1cdb198210cec533
Xiaojian.Tang <xiaojian.tang@partner.bmw.com>
2019-10-24 09:47:32 +0800

    sql

--- Files/sql1.sql
+++ Files/sql1.sql
@@ -1,2 +1,3 @@
 select Name,SelledOutLastMonth,SellingCount,ROUND(Price,2),(SelledOutLastMonth/(SellingCount + SelledOutLastMonth)) * 100 from Communitys 
+where UnitId = 1
 ORDER by (SelledOutLastMonth/(SellingCount + SelledOutLastMonth)) desc  ,Price asc 
